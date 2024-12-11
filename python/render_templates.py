2c93c45b3ae32c830d28db8e7c24e6bd5ee5da05
sgatewood <stg2bd@virginia.edu>
2024-12-11 10:35:48 -0500

    make script not depend on working dir

--- python/render_templates.py
+++ python/render_templates.py
@@ -6,11 +6,11 @@ import pdfkit
 import yaml
 from jinja2 import Template
 
-root_dir = pathlib.Path("..")
-templates_dir = root_dir / "templates"
-output_dir = root_dir / "rendered"
-assets_dir = root_dir / "assets"
-css_file = root_dir / "style.css"
+repo_root = pathlib.Path(__file__).parent.parent
+templates_dir = repo_root / "templates"
+output_dir = repo_root / "rendered"
+assets_dir = repo_root / "assets"
+css_file = repo_root / "style.css"
 
 
 class TemplateFile:
@@ -32,12 +32,12 @@ def setup_directories() -> None:
     if output_dir.exists():
         shutil.rmtree(output_dir)
     output_dir.mkdir()
-    (output_dir / css_file.name).symlink_to(css_file)  # Makes the HTML work locally
-    (output_dir / assets_dir.name).symlink_to(assets_dir)
+    (output_dir / css_file.name).symlink_to(css_file.relative_to(output_dir, walk_up=True))  # Makes the HTML work locally
+    (output_dir / assets_dir.name).symlink_to(assets_dir.relative_to(output_dir, walk_up=True))
 
 
 def render_templates() -> None:
-    resume_yaml = root_dir / "resume.yaml"
+    resume_yaml = repo_root / "resume.yaml"
     for file in templates_dir.glob('**/*'):
         output_file = output_dir / file.with_stem("resume").name
         TemplateFile(file).render_using(resume_yaml, output_file)
